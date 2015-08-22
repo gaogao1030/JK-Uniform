f9b25a110f8a1253b5b9d996c05ffeaa57184297
mac-gaogao <gaogao1130@gmail.com>
2015-08-23 00:53:21 +0800

    add paginator.rb

---
+++ _plugins/paginator.rb
@@ -0,0 +1,45 @@
+module Jekyll
+  module Paginate
+    class Pagination < Generator
+      # Paginates the blog's posts. Renders the index.html file into paginated
+      # directories, e.g.: page2/index.html, page3/index.html, etc and adds more
+      # site-wide data.
+      #
+      # site - The Site.
+      # page - The index.html Page that requires pagination.
+      #
+      # {"paginator" => { "page" => <Number>,
+      #                   "per_page" => <Number>,
+      #                   "posts" => [<Post>],
+      #                   "total_posts" => <Number>,
+      #                   "total_pages" => <Number>,
+      #                   "previous_page" => <Number>,
+      #                   "next_page" => <Number> }}
+      def paginate(site, page)
+        all_posts = site.site_payload['site']['posts'].reject { |post| post['hidden'] }
+       # top = []
+       # bottom = []
+       # all_posts.each do |post| #top标签置顶
+       #   if post.categories.include? "top"
+       #     top.push post
+       #   else
+       #     bottom.push post
+       #   end
+       #   all_posts = top + bottom
+       # end
+        pages = Pager.calculate_pages(all_posts, site.config['paginate'].to_i)
+        (1..pages).each do |num_page|
+          pager = Pager.new(site, num_page, all_posts, pages)
+          if num_page > 1
+            newpage = Page.new(site, site.source, page.dir, page.name)
+            newpage.pager = pager
+            newpage.dir = Pager.paginate_path(site, num_page)
+            site.pages << newpage
+          else
+            page.pager = pager
+          end
+        end
+      end
+    end
+  end
+end
