eb867d6c8f6098bc138d361a0c0727efefcaadc6
mac-gaogao <gaogao1130@gmail.com>
2015-10-14 22:22:29 +0800

    update at 2015-10-14 14:22:29 UTC

--- javascript/components/hover.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-  $("a.click-able").on("mouseover", function() {
-    return $(this).addClass("click-able-effect");
-  });
-  return $("a.click-able").on("mouseout", function() {
-    return $(this).removeClass("click-able-effect");
-  });
-});
